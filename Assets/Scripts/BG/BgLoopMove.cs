258c73aadc199655acee0cc4d91d26dd1aaab002
silentbo <1713598667@qq.com>
2017-07-28 17:33:00 +0800

    unity项目导入
    将这个仓库进行unity导入，使之可以使用unity直接打开这个项目，
    这个项目主要提交的是asset文件夹
    Library
    ProjectSettings
    文件夹不需要提交，
    当你运行过unity这个项目的时候，那么会出现obj文件夹
    这个文件夹也是不需要提交的

---
+++ Assets/Scripts/BG/BgLoopMove.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BgLoopMove : MonoBehaviour
+{
+
+    public float moveSpreed = 2.0f; // 移动速度
+
+    public float bgHight = 0f; // 背景高度
+
+    public Renderer rendererBg; // 为了获取图片的实际大小
+
+	// Use this for initialization
+	void Start (){
+	    this.rendererBg = this.transform.GetComponent<Renderer>(); // 获取图片的实际大小
+        print(this.rendererBg.bounds.extents.x);
+        print(this.rendererBg.bounds.extents.y);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        // 移动屏幕
+	    this.transform.Translate(Vector3.down * moveSpreed * Time.deltaTime);
+
+        // *2 是图片的实际高度，当图片所有都移除屏幕外的时候在将其设置回到屏幕上面，来进行循环
+	    if (this.transform.position.y <= -this.rendererBg.bounds.extents.y * 2) 
+            // 设置图片的位置，用来循环
+	        this.transform.position = new Vector3(this.transform.position.x,
+	            this.transform.position.y + this.rendererBg.bounds.extents.y * 2 * 2, this.transform.position.z);
+	}
+}

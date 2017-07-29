b7e8910d0d8c7fd77438626ffcdb2ff0a185c162
silentbo <1713598667@qq.com>
2017-07-29 20:30:11 +0800

    unity项目修改文件
    使用unity打开项目
    并实现某些功能
    本次实现的功能是;玩家可以通过点击左右两边的按钮来实现主角泡泡的左右移动

---
+++ Assets/Scripts/Button/BtnControlDirection.cs
@@ -0,0 +1,44 @@
+﻿using UnityEngine;
+using System.Collections;
+
+// 控制主角移动方向
+public class BtnControlDirection : MonoBehaviour {
+
+    // 控制方向的enum
+    public enum BtnControlDirectionType
+    {
+        BtnControlDirectionDefault, // 默认，没有方向
+        BtnControlDirectionLeft,    // 左
+        BtnControlDirectionRight,   // 右
+    }
+
+    public BtnControlDirectionType btnControlDirectionType = BtnControlDirectionType.BtnControlDirectionDefault; // 按钮类型
+
+    public GameObject player; // 泡泡 主角
+
+    public float playerMoveSpeed = 5.0f; // 移动速度
+
+    //public float speedMoveTemp = 
+
+    public Vector3 moveDirection = Vector3.zero; // 主角移动的方向
+
+    void OnMouseDrag()
+    {
+        print("-- silent -- OnMouseDrag ......");
+
+        switch (btnControlDirectionType)
+        {
+            case BtnControlDirectionType.BtnControlDirectionLeft:
+                moveDirection = Vector3.left;
+                break;
+            case BtnControlDirectionType.BtnControlDirectionRight:
+                moveDirection = Vector3.right;
+                break;
+            case BtnControlDirectionType.BtnControlDirectionDefault:
+                moveDirection = Vector3.zero;
+                break;
+        }
+
+        player.transform.Translate( moveDirection * playerMoveSpeed * Time.deltaTime);
+    }
+}
---
+++ Assets/Scripts/GameManage/GameManager.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+// 游戏管理类
+public class GameManager : MonoBehaviour {
+ 
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}

ad59ce7ace9149d1693a56251867778ef8ba1719
silentbo <1713598667@qq.com>
2017-07-31 23:56:12 +0800

    增加主角动画--待机和吃
    增加主角动画
    待机动画
    吃东西动画
    使用的是新的animator控件

---
+++ Assets/Scripts/Player/Player.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Player : MonoBehaviour {
+
+    public Animator animatorPlayer; // 泡泡动画
+    public bool isEatFinished; // 是否吃完了
+
+	// Use this for initialization
+	void Start () {
+        InvokeRepeating("Eat", 2.0f, 0.5f);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (!isEatFinished)
+            animatorPlayer.SetBool("eat", false);
+	}
+
+
+    private void Eat()
+    {
+        animatorPlayer.SetBool("eat", true);
+    }
+
+}

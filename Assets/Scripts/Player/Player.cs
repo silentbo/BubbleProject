845a20c1dde2cc516502e7791d12b9301db16bab
silentbo <1713598667@qq.com>
2017-08-02 00:31:14 +0800

    主角泡泡吃掉奖励泡泡实现
    实现功能：
    主角可以吃掉奖励的泡泡，并且播放吃掉动画，奖励生命值
    奖励泡泡跟随背景向下运动
    主角和奖励泡泡实现碰撞

--- Assets/Scripts/Player/Player.cs
+++ Assets/Scripts/Player/Player.cs
@@ -9,8 +9,6 @@ public class Player : MonoBehaviour {
 
 	// Use this for initialization
 	void Start () {
-        InvokeRepeating("Eat", 2.0f, 0.5f);
-        Invoke("CanelInvokeTemp", 5.0f);
 
 	}
 	
@@ -33,39 +31,26 @@ public class Player : MonoBehaviour {
 
     }
 
-    // 2d 碰撞触发函数
-    void OnCollisionEnter2D(Collider2D other)
-    {
-        print("-- silent -- other tag collision == " + other.tag);
-        switch (other.tag)
-        {
-            case "reward_bubble":
-                ColliderByRewardBubble(other);
-                break;
-        }
-
-    }
-
+    // 碰撞奖励泡泡事件
     private void ColliderByRewardBubble(Collider2D other2D)
     {
+        // 播放吃东西动画
+        EatRewardBubble();
+
+        // 获取奖励泡泡增加的生命值
         RewardBubble rewardBubble = other2D.GetComponent<RewardBubble>();
         if (!rewardBubble) return;
         gameManager.IncreasePlayerLife(rewardBubble.hpRewardBubble);
+
+        // 删除奖励泡泡
+        Destroy(other2D.gameObject);
+        //other2D.gameObject.SetActive(false);
     }
 
 
-    private void Eat()
+    private void EatRewardBubble()
     {
-        //animatorPlayer.SetBool("eatFinish", true);
-        //animatorPlayer.Play("player_eat");
         animatorPlayer.PlayInFixedTime("player_eat");
-        //animatorPlayer.SetBool("eatStart", true);
-    }
-
-    private void CanelInvokeTemp()
-    {
-        CancelInvoke("Eat");
-        CancelInvoke("CanelInvokeTemp");
     }
 
 }
--- Assets/Scripts/Reward/RewardBubble.cs
+++ Assets/Scripts/Reward/RewardBubble.cs
@@ -7,6 +7,9 @@ public class RewardBubble : MonoBehaviour
     public float hpRewardBubble = 1.0f; // 泡泡奖励生命值
     public float scaleRandomRewardBubble = 0.0f; // 泡泡缩放比例
 
+    public float speedMoveDown = 2.0f; // 下落速度，跟背景下落速度一致
+    public bool isMoveDown = true; // 是否向下移动
+
 	// Use this for initialization
 	void Start (){
 	    InitRewardBubble();
@@ -14,12 +17,21 @@ public class RewardBubble : MonoBehaviour
 	
 	// Update is called once per frame
 	void Update () {
-	
-	}
+        MoveDown();
+    }
 
+    // 初始化泡泡的生命和scale
     private void InitRewardBubble(){
         scaleRandomRewardBubble = Random.Range(0.2f, 1.0f);
         this.transform.localScale = new Vector3(scaleRandomRewardBubble, scaleRandomRewardBubble, scaleRandomRewardBubble);
         hpRewardBubble = scaleRandomRewardBubble;
+    }
+
+    private void MoveDown()
+    {
+        if (!isMoveDown)
+            return;
+
+        this.transform.Translate(Vector3.down * speedMoveDown * Time.deltaTime);
     }
 }

b2e1b9011fb755378e2e84af6524ea78b0687121
silentbo <1713598667@qq.com>
2017-09-12 17:28:21 +0800

    主界面增加一些按钮
    主界面增加一些按钮
    在游戏开始界面中，
    增加一些功能按钮，
    实现了场景的跳转

---
+++ Assets/Scripts/Button/BtnManagerMain.cs
@@ -0,0 +1,49 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class BtnManagerMain : MonoBehaviour
+{
+
+    public bool isPlaying = true;  // 是否正在游戏
+    public bool isTouching = true; // 是否可以点击
+
+    public Transform tran;
+
+    // 进入游戏场景按钮
+    public void BtnClickOfPlayGame()
+    {
+        SceneManager.LoadScene("Bubble_Playing"); // 跳转到游戏场景中
+    }
+
+    // 音乐开关
+    public void BtnClickOfMusic()
+    {
+        
+    }
+
+    // 声音开关
+    public void BtnClickOfSound()
+    {
+
+    }
+
+    // 分享
+    public void BtnClickOfShare()
+    {
+        
+    }
+
+    // 帮助
+    public void BtnClickOfHelp()
+    {
+        
+    }
+
+    // 信息
+    public void BtnClickOfInfo()
+    {
+ 
+    }
+
+
+}
---
+++ Assets/Scripts/PublicTemplate/RotateAroundTemplate.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+
+// 物体绕着目标物体旋转类
+public class RotateAroundTemplate : MonoBehaviour
+{
+    public bool isAnimation = true;    // 是否播放动画
+    public bool isPositive = true;     // 是否是正方向(顺时针)
+
+    public float speedRotateAround = 30.0f;  // 旋转的速度
+
+    public Transform targetRotateAround; // 绕着旋转的物体
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+	    if (isAnimation)
+	    {
+            this.transform.RotateAround(targetRotateAround.position, isPositive ? Vector3.forward : Vector3.back, speedRotateAround * Time.deltaTime);
+	    }
+	}
+}
